1f22d7ce2431fcc89b47f25f56dfbc4f2e988656
Daniel Weibel <danielmweibel@gmail.com>
2020-10-07 17:14:06 +0200

    Print entire github object

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const github= JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`github: ${github}`);
 } catch (error) {
   core.setFailed(error.message);
 }
