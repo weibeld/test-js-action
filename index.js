dd56d9dc4ea0e1d26b1cce0a49ff9e3778b8c954
Daniel Weibel <danielmweibel@gmail.com>
2020-10-10 12:45:38 +0200

    Output secrets context in composite action

--- index.js
+++ index.js
@@ -2,21 +2,28 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const { exec, execSync } = require("child_process");
 
+function fail(err) {
+  core.setFailed(err);
+}
+
+function run(cmd) {
+  execSync(cmd);
+}
+
 try {
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
-
-  execSync("pwd", {stdio: 'inherit'});
+  //run("npm install -g ajv-cli");
+  s = execSync("date -x");
+  //console.log(s.toString());
  
   // Get the JSON webhook payload for the event that triggered the workflow
   //const obj = JSON.stringify(github, undefined, 2)
   //console.log(`github: ${obj}`);
 } catch (err) {
-  fail(err)
+  console.log(err.message)
+  //fail(err)
 }
 
-function fail(err) {
-  core.setFailed(err);
-}
